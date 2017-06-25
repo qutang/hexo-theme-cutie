ad5457f85a643b9ebd0c991b56582719891c703e
Qu Tang <tqshelly@gamil.com>
2017-06-25 17:00:45 -0400

    Change behavior when first detect touch as a workaround to fix #2.

---
+++ source/js/theme.js
@@ -0,0 +1,24 @@
+function disableHoverOnTouch() {
+	window.addEventListener('touchstart', function() {
+		// the user touched the screen!
+
+		// remove all Card-hover class
+		var elems = document.querySelectorAll(".Card-hover");
+		[].forEach.call(elems, function(el) {
+			el.classList.remove("Card-hover");
+		});
+
+		// set to display exerpt and title together in cards
+		elems = document.querySelectorAll(".Card-excerpt");
+		[].forEach.call(elems, function(el) {
+			el.classList.add("Card-excerpt-notouch")
+		});
+		// set to hide (tags) in cards, as it is involved in exerpt already
+		elems = document.querySelectorAll(".Card-title-tags");
+		[].forEach.call(elems, function(el) {
+			el.style.display = "none";
+		});
+	});
+}
+
+disableHoverOnTouch();
