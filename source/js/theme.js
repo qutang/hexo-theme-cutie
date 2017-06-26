c78bdbf4374a4a418666acfbe1623d954cb85ab8
Qu Tang <tqshelly@gamil.com>
2017-06-26 01:03:16 -0400

    Close #12, by default enabled

--- source/js/theme.js
+++ source/js/theme.js
@@ -6,22 +6,25 @@ function disableHoverOnTouch() {
 }
 
 function changeLayoutOnTouchScreen() {
-	// remove all Card-hover class
-	var elems = document.querySelectorAll(".Card-hover");
-	[].forEach.call(elems, function(el) {
-		el.classList.remove("Card-hover");
-	});
+	if('ontouchstart' in window        // works on most browsers 
+      || navigator.maxTouchPoints){
+		// remove all Card-hover class
+		var elems = document.querySelectorAll(".Card-hover");
+		[].forEach.call(elems, function(el) {
+			el.classList.remove("Card-hover");
+		});
 
-	// set to display exerpt and title together in cards
-	elems = document.querySelectorAll(".Card-excerpt");
-	[].forEach.call(elems, function(el) {
-		el.classList.add("Card-excerpt-notouch")
-	});
-	// set to hide (tags) in cards, as it is involved in exerpt already
-	elems = document.querySelectorAll(".Card-title-tags");
-	[].forEach.call(elems, function(el) {
-		el.style.display = "none";
-	});
+		// set to display exerpt and title together in cards
+		elems = document.querySelectorAll(".Card-excerpt");
+		[].forEach.call(elems, function(el) {
+			el.classList.add("Card-excerpt-notouch")
+		});
+		// set to hide (tags) in cards, as it is involved in exerpt already
+		elems = document.querySelectorAll(".Card-title-tags");
+		[].forEach.call(elems, function(el) {
+			el.style.display = "none";
+		});
+	}
 }
 
 disableHoverOnTouch();
