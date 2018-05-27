0a3887896cd7c35b864831798ac01205991ddbb1
qutang <tqshelly@gmail.com>
2018-05-27 00:58:17 -0400

    Add functionality to mockup
    1. Auto sparkle icons based on post content
    2. Helpers to extract useful info from post content
    3. Auto swiper bullets based on post content

--- scripts/extractor.js
+++ scripts/extractor.js
@@ -1,3 +1,67 @@
-hexo.extend.helper.register("get_images", function(post){
-  
-})
+const cheerio = require('cheerio')
+
+hexo.extend.helper.register("get_post_images", function(post, root_path){
+  var images = []
+  console.log('extract images from post front matter (photos)');
+  front_matter_images = post.photos;
+  console.log('extract images from post content');
+  var content = post.content;
+  var content_images = get_images_from_html_string(content);
+  images = images.concat(front_matter_images.concat(content_images));
+  images = images.map(function(path){
+    if(path.startsWith('/')){
+      return path;
+    }else if(path.search('http:|https:|ftp:|sftp:') != -1){
+      return path;
+    }else{
+      return post.path + path;
+    }
+  });
+  console.log(images);
+  return images;
+});
+
+hexo.extend.helper.register("get_post_codes", function(post){
+  console.log('extract codes from post content');
+  var codes = get_codes_from_html_string(post.content);
+  console.log(codes);
+  return codes;
+});
+
+hexo.extend.helper.register("has_math_equations", function(post){
+  return has_equations_in_html(post.content);
+});
+
+var has_equations_in_html = function(content){
+  var eq_pattern = /\$.*\$/gmiu
+  console.log('parse math');
+  console.log(content.match(eq_pattern));
+  // console.log(content);
+  return content.match(eq_pattern) != null
+}
+
+var get_images_from_html_string = function(content){
+  var image_pattern = /src=['"]([-_:/\.a-zA-z0-9]+\.(jpg|png|jpeg|svg|gif|bmp|cur|tiff|webp|dds))['"]/gmiu
+  var matches;
+  var images = [];
+  while( (matches = image_pattern.exec(content)) != null) {
+    var path = matches[1];
+    images.push(path);
+  }
+  return images;
+}
+
+var get_codes_from_html_string = function(content){
+  const $ = cheerio.load(content);
+  var codes = [];
+  $('figure.highlight .code').each(function(i, el){
+    var lang = $(this).parents('figure.highlight').attr('class').split(' ')[1]
+    var lines = $(this).find('.line').length;
+    codes.push({
+      'content': $(this).html(),
+      'lang': lang,
+      'lines': lines
+    });
+  });
+  return codes;
+}
