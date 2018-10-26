b73de2b8c809a73c3812888835bbf0922c7df187
qutang <tqshelly@gmail.com>
2018-10-26 19:26:51 -0400

    v2.1.2
    Minor adjust colors of meta icons.

--- source/js/theme.js
+++ source/js/theme.js
@@ -1,33 +1,33 @@
 function disableHoverOnTouch() {
-	window.addEventListener('touchstart', function() {
+	window.addEventListener('touchstart', function () {
 		// the user touched the screen!
 		changeLayoutOnTouchScreen();
 	});
 }
 
 function onClickMenuIcon(event) {
-		if('ontouchstart' in document.documentElement){
-			event.preventDefault();
-			event.stopPropagation();
-			var elems = document.getElementsByClassName('nav-more-menu');
-			console.log(elems.length);
-			[].forEach.call(elems, function(el) {
-				console.log(el.style.display);
-				if(!el.style.display || el.style.display == 'none')
-					el.style.display = "block";
-				else
-					el.style.display = 'none';
-			});
-		}
-}
-
-function onClickNavMenuClose(event) {
-	if('ontouchstart' in document.documentElement){
+	if ('ontouchstart' in document.documentElement) {
 		event.preventDefault();
 		event.stopPropagation();
 		var elems = document.getElementsByClassName('nav-more-menu');
 		console.log(elems.length);
-		[].forEach.call(elems, function(el) {
+		[].forEach.call(elems, function (el) {
+			console.log(el.style.display);
+			if (!el.style.display || el.style.display == 'none')
+				el.style.display = "block";
+			else
+				el.style.display = 'none';
+		});
+	}
+}
+
+function onClickNavMenuClose(event) {
+	if ('ontouchstart' in document.documentElement) {
+		event.preventDefault();
+		event.stopPropagation();
+		var elems = document.getElementsByClassName('nav-more-menu');
+		console.log(elems.length);
+		[].forEach.call(elems, function (el) {
 			el.style.display = 'none';
 		});
 	}
@@ -39,9 +39,9 @@ function toggleTOC(event) {
 	event.stopPropagation();
 	var elems = document.querySelectorAll('#footer-nav .toc-container');
 	console.log(elems.length);
-	[].forEach.call(elems, function(el) {
+	[].forEach.call(elems, function (el) {
 		console.log(el.style.display);
-		if(!el.style.display || el.style.display == 'none')
+		if (!el.style.display || el.style.display == 'none')
 			el.style.display = "block";
 		else
 			el.style.display = 'none';
@@ -53,25 +53,25 @@ function closeTOC(event) {
 	event.stopPropagation();
 	var elems = document.querySelectorAll('#footer-nav .toc-container');
 	console.log(elems.length);
-	[].forEach.call(elems, function(el) {
+	[].forEach.call(elems, function (el) {
 		console.log(el.style.display);
 		el.style.display = 'none';
 	});
 }
 
 function changeLayoutOnTouchScreen() {
-	if('ontouchstart' in window        // works on most browsers 
-      || navigator.maxTouchPoints){
+	if ('ontouchstart' in window        // works on most browsers 
+		|| navigator.maxTouchPoints) {
 		// remove all Card-hover class
 		console.log('trigger change layout')
 		var elems = document.querySelectorAll(".Card-hover");
-		[].forEach.call(elems, function(el) {
+		[].forEach.call(elems, function (el) {
 			el.classList.remove("Card-hover");
 		});
 
 		// set to display exerpt and title together in cards
 		elems = document.querySelectorAll(".Card-excerpt");
-		[].forEach.call(elems, function(el) {
+		[].forEach.call(elems, function (el) {
 			el.classList.add("Card-excerpt-touch");
 			el.classList.remove('Card-excerpt');
 		});
@@ -81,12 +81,12 @@ function changeLayoutOnTouchScreen() {
 function initHeadroom() {
 	var myElement = document.querySelector("header");
 	// construct an instance of Headroom, passing the element
-	var headroom  = new Headroom(myElement, {offset: 40, tolerance: 30});
+	var headroom = new Headroom(myElement, { offset: 40, tolerance: 30 });
 	// initialise
 	headroom.init();
 
 	var footernav = document.querySelector('#footer-nav');
-	var headroom3 = new Headroom(footernav, {offset: 40, tolerance: 30});
+	var headroom3 = new Headroom(footernav, { offset: 40, tolerance: 30 });
 	headroom3.init();
 }
 
